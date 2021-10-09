dd0b755b3a9ec9aaf81d9156dee3de6e3169fb0c
Mia Walsh <miawalsh00@gmail.com>
2021-10-09 15:33:49 -0400

    Added debt list to Housemate class/constructor.

--- src/Domain/Housemate.java
+++ src/Domain/Housemate.java
@@ -4,10 +4,12 @@ public class Housemate {
 
     String name;
     String housemateId;
+    Arraylist<Debt> debt;
 
     public Housemate(String name, String housemateId){
         this.name = name;
         this.housemateId = housemateId;
+        debt = new Arraylist<Debt>;
     }
 
 
