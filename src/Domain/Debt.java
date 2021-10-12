bc9442fe65d2f41850111910028c3a3a30962479
Mia Walsh <miawalsh00@gmail.com>
2021-10-12 17:48:41 -0400

    Changed attributes to private, added get methods for attributes.

--- src/Domain/Debt.java
+++ src/Domain/Debt.java
@@ -1,15 +1,21 @@
 package Domain;
 
 public class Debt {
-    boolean isPaid = false;
-    Housemate debtor;
-    Housemate creditor;
-    String itemName;
-    float owed;
+    private boolean isPaid = false;
+    private Housemate debtor;
+    private Housemate creditor;
+    private String itemName;
+    private float owed;
+
     public Debt(Housemate creditor, Housemate debtor, float owed, String itemName){
         this.debtor = debtor;
         this.creditor = creditor;
         this.owed = owed;
         this.itemName = itemName;
     }
+
+    public Housemate getDebtor(){return debtor;}
+    public Housemate getCreditor(){return creditor;}
+    public String getItemName(){return itemName;}
+    public float getOwed(){return owed;}
 }
