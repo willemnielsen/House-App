eca7545ca6b4fea804c965c00e919eed580a6030
Mia Walsh <miawalsh00@gmail.com>
2021-10-11 22:17:21 -0400

    Added myTransactions() and myBalance() methods.

--- src/Domain/Housemate.java
+++ src/Domain/Housemate.java
@@ -4,7 +4,7 @@ public class Housemate {
 
     String name;
     String housemateId;
-    Arraylist<Debt> debt;
+    Arraylist<Debt> debtlist;
 
     public Housemate(String name, String housemateId){
         this.name = name;
@@ -12,5 +12,23 @@ public class Housemate {
         debt = new Arraylist<Debt>;
     }
 
+    public void myTransactions(){
+        for (Debt debt: debtlist
+             ) {
+            System.out.println("You owe " + debt.creditor + " " + debt.owed + " for " + debt.name + ".\n");
+        }
+    }
 
+    public void myBalance(){
+        private float credit = 0;
+        private float owed = 0;
+        for (Debt debt: debtlist)
+             ) {
+    if(debt.creditor.name.equal(this.name))
+            credit += debt.owed;
+        else owed += debt.owed;
+
+    }
+        System.out.println("Your balance is " + credit-debt + ".");
+    }
 }
