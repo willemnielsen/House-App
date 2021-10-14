19f402e9c7d9dd14c02cf18a31caff5af77bbd53
Kishan Patel <31-kpatel@users.noreply.gitlab.cs.vassar.edu>
2021-10-14 21:08:38 +0000

    Update TerminalController.java

--- src/UI/TerminalController.java
+++ src/UI/TerminalController.java
@@ -69,6 +69,9 @@ public class TerminalController implements Runnable{
                     houseController.checkout(paymentType, askForHouseMate(houseController));
                     System.out.println(houseController.houseTransactions());
 
+                    //
+                    // Print blanace for house
+                    //
 
 
                     break;
