5f2106c67eb8d49fd368174dc8653ee62066db41
kishan <kpatel@vassar.edu>
2021-11-09 14:57:29 -0500

    working previous button, opens main screen from add item fragment

--- app/src/main/java/edu/vassar/cmpu/test/ControllerActivity.java
+++ app/src/main/java/edu/vassar/cmpu/test/ControllerActivity.java
@@ -4,23 +4,42 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import edu.vassar.cmpu.test.domain.HouseController;
 import edu.vassar.cmpu.test.view.addItemView.AddItemFragment;
+import edu.vassar.cmpu.test.view.addItemView.IAddItemView;
 import edu.vassar.cmpu.test.view.homeScreen.MainScreenFragment;
 
-public class ControllerActivity extends AppCompatActivity {
+public class ControllerActivity extends AppCompatActivity implements IAddItemView.Listener {
     //extends makes this class an activity
 
+    private HouseController houseController;
     private IMainView mainView;
 
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
 
+        houseController = new HouseController("TH 84");
+
         mainView = new MainView(this);
         setContentView(mainView.getRootView());
 
-        this.mainView.displayFragment(new MainScreenFragment());//displays the add Item Fragment
+        this.mainView.displayFragment(new AddItemFragment(this));//displays the add Item Fragment
     }
 
 
+    @Override
+    public void onAddedItem(String name, int quantity, IAddItemView addItemView) {
+        houseController.addLineItemToShoppingList(quantity, name, 0.0f, null);
+        addItemView.updateDisplay(houseController.getHouse().getShoppingList());
+    }
 
+    @Override
+    public void onPreviousInAddItemFragment() {
+        this.openHomeScreen();
+    }
+
+
+    public void openHomeScreen() {
+        this.mainView.displayFragment(new MainScreenFragment());
+    }
 }
--- app/src/main/java/edu/vassar/cmpu/test/view/addItemView/IAddItemView.java
+++ app/src/main/java/edu/vassar/cmpu/test/view/addItemView/IAddItemView.java
@@ -1,5 +1,14 @@
 package edu.vassar.cmpu.test.view.addItemView;
 
+import edu.vassar.cmpu.test.domain.ShoppingList;
+
 public interface IAddItemView {
+
+    interface Listener{
+        void onAddedItem(String name, int quantity, IAddItemView addItemView);
+        void onPreviousInAddItemFragment();
+    }
+
+    void updateDisplay(ShoppingList shoppingList);
 
 }
