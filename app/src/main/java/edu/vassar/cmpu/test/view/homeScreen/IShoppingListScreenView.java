869bbe41f807b2265513b2d0cc5bf2bfa35d5ce2
Mia Walsh <miawalsh00@gmail.com>
2021-11-11 14:54:52 -0500

    Added temp open calendar screen button.

--- app/src/main/java/edu/vassar/cmpu/test/view/homeScreen/IShoppingListScreenView.java
+++ app/src/main/java/edu/vassar/cmpu/test/view/homeScreen/IShoppingListScreenView.java
@@ -7,6 +7,7 @@ public interface IShoppingListScreenView {
 
     interface Listener{
         void onAddItem();
+        void onOpenCalendarScreen();
     }
 
     void updateDisplay(ShoppingList shoppingList);
