0bf3cd978938f534301e9d8b52c627d82cc3cb03
willemnielsen <wnielsen@vassar.edu>
2021-11-14 14:12:54 -0500

    Removed Event.recurrence

--- app/src/main/java/edu/vassar/cmpu/test/domain/Event.java
+++ app/src/main/java/edu/vassar/cmpu/test/domain/Event.java
@@ -10,15 +10,13 @@ public class Event {
     private Time startTime;
     private Time endTime;
     private ArrayList<Housemate> housemates;
-    private String recurrence;
 
-    public Event(String name, Date date, Time startTime, Time endTime,  ArrayList<Housemate> housemates, String recurrence) {
+    public Event(String name, Date date, Time startTime, Time endTime,  ArrayList<Housemate> housemates) {
         this.name = name;
         this.date = date;
         this.startTime = startTime;
         this.endTime = endTime;
         this.housemates = housemates;
-        this.recurrence = recurrence;
     }
 
     public ArrayList<Housemate> getHousemates() {return housemates;}
@@ -26,7 +24,6 @@ public class Event {
     public Date getDate() {return date;}
     public Time getStartTime() {return startTime;}
     public Time getEndTime() {return endTime;}
-    public String getRecurrence() {return recurrence;}
     public String toString(){
         return name + " from " + startTime + " to " + endTime;
     }
