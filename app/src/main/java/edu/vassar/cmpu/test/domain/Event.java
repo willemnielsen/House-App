b36f24b5b784475a6e205bb26ff9b6442dba26f8
Mia Walsh <miawalsh00@gmail.com>
2021-11-15 18:21:07 -0500

    Small format adjustments.

--- app/src/main/java/edu/vassar/cmpu/test/domain/Event.java
+++ app/src/main/java/edu/vassar/cmpu/test/domain/Event.java
@@ -27,12 +27,12 @@ public class Event {
     public String toString() {
         String print = "";
         print += name + " from ";
-        if (startTime.getHours() == 0) {
-            print += 12 + ":" + startTime.getMinutes() + "AM to ";
+        if (startTime.getHours() == 23) {
+            print += 12 + ":" + String.format("%02d", startTime.getMinutes()) + "AM to ";
         } else if (startTime.getHours() < 12) {
             print += (startTime.getHours()+1) + ":" + String.format("%02d", startTime.getMinutes()) + "AM to ";
         } else print += (startTime.getHours()-11) + ":" + String.format("%02d", startTime.getMinutes()) + "PM to ";
-        if (endTime.getHours() == 0) {
+        if (endTime.getHours() == 23) {
             print += 12 + ":" + String.format("%02d", endTime.getMinutes()) + "AM.";
         } else if (endTime.getHours() < 12) {
             print += (endTime.getHours()+1) + ":" + String.format("%02d", endTime.getMinutes()) + "AM. \n";
